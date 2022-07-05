112ac7410c884f3ce004a42a62722e421795c76a
Barbara Broto <babr230@vidalibarraquer.net>
2022-07-05 10:25:12 +0200

    Mi primer commit

---
+++ src/mainApp.java
@@ -0,0 +1,9 @@
+
+public class mainApp {
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		// syso ctrl + espacio para autocompletar
+		System.out.println("Hola Mundo!!");
+		
+	}
+}
